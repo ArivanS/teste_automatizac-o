e30825eba96e43a7888879ce81db1373eda35de0
ArivanS <arivan.damiao@ba.estudante.senai.br>
2026-09-15 14:36:10 -0300

    Corrige regressao na soma

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,4 +1,13 @@
-from app.calculadora import somar
+from app.calculadora import somar, subtrair, calcular_desconto
+
 
 def test_somar():
     assert somar(2, 3) == 5
+
+
+def test_subtrair():
+    assert subtrair(5, 3) == 2
+
+
+def test_calcular_desconto():
+    assert calcular_desconto(100, 10) == 90
